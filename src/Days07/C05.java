a18c6d333d5aec7a2f6c58185d592194554df5ed
HalikarnasBalikcisi <121846014+AyRamazan@users.noreply.github.com>
2023-08-19 10:57:29 +0300

    oo

--- src/Days07/C05.java
+++ src/Days07/C05.java
@@ -4,5 +4,10 @@ public class C05 {
     public static void main(String[] args) {
 
         System.out.println("hello kaloo");
+
+        System.out.println("aeaea");
+
+        System.out.println("aeae");
+
     }
 }
