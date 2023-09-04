326a9ed2730aae6d03810de8df278cae7aa4b7e8
HalikarnasBalikcisi <121846014+AyRamazan@users.noreply.github.com>
2023-09-04 23:32:27 +0300

    ooo

--- src/Days07_Ternary_SwitchStatements/C002_Swich.java
+++ src/Days07_Ternary_SwitchStatements/C002_Swich.java
@@ -1,7 +1,9 @@
 package Days07_Ternary_SwitchStatements;
 
-public class C12_BABOX {
+public class C002_Swich {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        System.out.println("aeae");
     }
 }
---
+++ src/Days08/C01.java
@@ -0,0 +1,8 @@
+package Days08;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        System.out.println("baby");
+    }
+}
