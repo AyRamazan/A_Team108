9ce55c4c8ca8a6576d4f0160732754dbee1023c7
HalikarnasBalikcisi <121846014+AyRamazan@users.noreply.github.com>
2023-09-16 07:59:53 +0300

    1609

--- src/Days10_StringManipulations/C03_ReplaceAll.java
+++ src/Days10_StringManipulations/C03_ReplaceAll.java
@@ -5,6 +5,7 @@ public class C03_ReplaceAll {
     public static void main(String[] args) {
 
 
+        /*
         String str="Ja1va 56Guz,zel-dir";
         //bu metindeki sayilardan kurtulun
 
@@ -13,6 +14,8 @@ public class C03_ReplaceAll {
         System.out.println(str.replaceAll("\\d",""));
 
         str="Ja1va4 565Guz,z6el-578dir";
+
+         */
 
         /*
         replace method'u zaten istenen degisikligi verilen özellige uyan tum degerler icin yapar
@@ -24,6 +27,7 @@ public class C03_ReplaceAll {
         \\d: butun digit'ler
 
          */
+        /*
 
         System.out.println(str.replaceAll("\\d",""));
 
@@ -45,6 +49,55 @@ public class C03_ReplaceAll {
         str= str.replace("5"," ");
 
         System.out.println(str);
+
+        */
+
+        // bu metindeki sayilardan kurtulun
+
+        String str= "Ja1v4a 56G8uz,6el-di24r";
+        System.out.println(str.replace("1", "").replace("5","").replace("6",""));
+
+        /*
+        Replace method'u zaten istenen degisikligi verilen ozellige uyan tum degerler icin yapar
+
+        eger genellemeyi butun sayilari, butun space'leri , butun ozel karakterler gibi genisletmek istersek
+        replace() yerine replaceAll() method'unu kullaniriz
+
+        Java bu genellemeleri yazabilmemiz icin regex (regular expressions) tanimlamistir
+
+        \\d: butun digitler icin
+         */
+
+        str="Ja/va,;: ogren,./*dikce guzelle?==00098siyor23;.";
+
+
+        str=str.replaceAll("\\d","");
+        System.out.println(str);
+
+        System.out.println(str.replaceAll("\\d", ""));//Java Guz,zel-dir
+
+        // ozel karakterlerden de kurtulun
+        // space de ozel karakter olduğundan, spacenin yok olmaması icin
+        // önce space yerine metinde olmayan bir deger atayalim
+
+        str=str.replaceAll(" ","5");
+
+        str=str.replaceAll("\\W","");
+
+        str=str.replaceAll("5"," ");
+        System.out.println(str);
+
+        //buradaki karakter sayisini bulalim
+        String input3="Ali Can, Merve Star, Mark Tom";
+
+        input3=input3.replaceAll("\\W","");
+        System.out.println(input3);
+        System.out.println(input3.length());
+
+
+
+
+
 
     }
 }
---
+++ src/Days10_StringManipulations/C04_ReplaceFirst.java
@@ -0,0 +1,22 @@
+package Days10_StringManipulations;
+
+public class C04_ReplaceFirst {
+    public static void main(String[] args) {
+
+        String str= "Java heyecandir";
+
+        //eger tum a'lari degil de sadece ilk a'yi degistirmek istersek
+
+        System.out.println(str.replaceFirst("a","A"));
+
+        //ilk harf veya rakami sifir yap
+
+        System.out.println(str.replaceFirst("\\w","*"));
+
+
+
+
+
+
+    }
+}
