57e2415d1b72087344e9b31fb92358947fbb2176
HalikarnasBalikcisi <121846014+AyRamazan@users.noreply.github.com>
2023-12-16 18:53:00 +0300

    16/12

--- src/Days11_ForLoops/C03_AsalSayi.java
+++ src/Days11_ForLoops/C03_AsalSayi.java
@@ -1,5 +1,8 @@
 package Days11_ForLoops;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class C03_AsalSayi {
     public static void main(String[] args) {
 
@@ -99,5 +102,86 @@ public class C03_AsalSayi {
         System.out.println("hello");
 
         System.out.println("hello");
+
+        int numara= 9;
+
+        switch (numara){
+            case 1:
+                System.out.println("Pazartesi");
+                break;
+            case 2:
+                System.out.println("Salı");
+                break;
+            case 3:
+                System.out.println("Carsamba");
+                break;
+            case 4:
+                System.out.println("perşembe");
+                break;
+            case 5:
+                System.out.println("cuma");
+                break;
+            case 6:
+                System.out.println("Cumartesi");
+                break;
+            case 7:
+                System.out.println("Pazar");
+                break;
+            default :
+                System.out.println("gecersiz sayi");
+
+        }
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen gün ismini giriniz");
+        String girilecekGun= scan.next();
+        String kullanilacakGun= girilecekGun.toLowerCase();
+
+        switch (kullanilacakGun){
+            case "pazartesi":
+                System.out.println("Simdi calisma zamani tatile 5 gün var");
+                break;
+            case "sali":
+                System.out.println("Simdi calisma zamani tatile 4 gün var");
+                break;
+            case "carsamba":
+                System.out.println("Simdi calisma zamani tatile 3 gün var");
+                break;
+            case "persembe":
+                System.out.println("Simdi calisma zamani tatile 2 gün var");
+                break;
+            case "cuma":
+                System.out.println("Simdi calisma zamani tatile 1 gün var");
+                break;
+            case "cumartesi":
+                System.out.println("Simdi tatil zamani");
+                break;
+            case "pazar":
+                System.out.println("Simdi tatil zamani");
+                break;
+            default:
+                System.out.println("yanlis gün ismi");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
