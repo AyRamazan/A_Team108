13e32a0931a84b2cfd24603df1e340dc9efaa2c6
HalikarnasBalikcisi <121846014+AyRamazan@users.noreply.github.com>
2023-12-27 15:16:20 +0300

    27/12

---
+++ src/Days12_MethodCreation/C01_Method.java
@@ -0,0 +1,31 @@
+package Days12_MethodCreation;
+
+public class C01_Method {
+    public static void main(String[] args) {
+
+
+        //method'lar bazen cok basit islemleri, bazen de kompleks islemleri yapabilir
+        // biz o islemleri nasil yapabiliriz diye dusunmek yerine hazir method'u kullanmayi tercih ederiz
+        // bizden sonra kodu inceleyecek kisiler de hazir method isimlerinden ne yapmak istedigimizi kolayca anlayabilirler
+        //ayrica kodu her yazdigimizde yapabilecegimz muhtemel hatalardan kurtulmus oluruz
+
+       String str="Java'da yolu yariladik";
+
+       str.isEmpty(); // boolean sonuc dondu ama kullanmadik
+
+        //Method'un dondurdugu sonucu kullanmak isterseniz
+        //ya yazdirirsiniz
+
+        System.out.println(str.isBlank());
+
+        //ya da donen sonucu ileride kullanacaksak atama yapabiliriz
+
+        String buyukHarfliHali=str.toUpperCase();
+
+
+
+
+
+
+    }
+}
