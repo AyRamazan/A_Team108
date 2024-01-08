fe124f7ce3b1aea7d291b3ae352e4ef0f9a3635f
HalikarnasBalikcisi <121846014+AyRamazan@users.noreply.github.com>
2024-01-08 08:36:00 +0300

    08/01

---
+++ src/Days13_MethodOverLoading_WhileLoop/C04_MethodOverLoading_WhileLoop.java
@@ -0,0 +1,33 @@
+package Days13_MethodOverLoading_WhileLoop;
+
+public class C04_MethodOverLoading_WhileLoop {
+    public static void main(String[] args) {
+
+        // 2 basamakli sayilardan 7 ile bolunebilenleri yazdirin
+
+        for (int i = 10; i <100 ; i++) {
+
+            if (i%7==0){
+                System.out.print(i+" ");
+            }
+
+        }
+
+        System.out.println("");
+
+        // ayni soruyu whileLoop ile yapalim
+
+        int sayi=10;
+
+        while (sayi<100){
+            if (sayi%7==0){
+                System.out.print(sayi+" ");
+            }
+            sayi++;
+
+        }
+
+        //for loop ile yapılabilecek bir soruyu illa da while loop ile yapmak daha zahmetli
+
+    }
+}
